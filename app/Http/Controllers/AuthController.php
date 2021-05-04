f2ff3f307079d9fc9b84112434c984f10446b29f
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2021-05-04 05:20:34 +0000

    Swagger追加

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,59 @@ use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/login",
+     *     operationId="post_login",
+     *     tags={"auth"},
+     *     summary="ログイン",
+     *     description="ログイン処理",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             required={"email", "password"},
+     *             @OA\Property(
+     *                 property="email",
+     *                 type="string",
+     *                 example="hoge@hoge.com",
+     *                 description="メールアドレス"
+     *             ),
+     *             @OA\Property(
+     *                 property="password",
+     *                 type="string",
+     *                 example="password",
+     *                 description="パスワード"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="成功",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="access_token",
+     *                 type="string",
+     *                 description="アクセストークン",
+     *                 example="1:xxxxxx"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 description="認証エラー"
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function login(LoginRequest $request)
     {
         if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
---
+++ app/Http/Controllers/swagger.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     title="Swaggerサンプル",
+ *     version="0.1"
+ * )
+ */
+
+/**
+ * @OA\Server(
+ *     url="/api"
+ * )
+ */
